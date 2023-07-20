54ff34138f69f019fc8f24f15fa4f34b4c89f3ed
Eduardo <edu.he.dev@gmail.com>
2023-07-20 10:21:05 -0300

    add bootstrap

--- app/Utils/View.php
+++ app/Utils/View.php
@@ -4,16 +4,43 @@ namespace App\Utils;
 
 class View
 {
+
+  /**
+   * Método responspável por retornar o conteúdo de uma view
+   *
+   * @param string $view
+   * @return string
+   **/
+
   private static function getContentView(string $view): string
   {
     $file = __DIR__ . "/../../resources/view/$view.html";
     return file_exists($file) ? file_get_contents($file) : '';
   }
-
-  public static function render($view)
+  /**
+   * Método responspável por retornar o conteúdo rednderizado 
+   * 
+   * @param string 
+   * @param array
+   * @return string
+   **/
+  public static function render(string $view, array $vars = []): string
   {
+    // Obtem as chaves do array de variaveis
+    $keys = array_keys($vars);
+
+    // Monta o placeholder utilizando as chaves do array de variaveis 
+    $createPlaceholder = function ($key) {
+      return "{{" . $key . "}}";
+    };
+
+    // Aplica a função $createPlaceholder para cada elemento do array de variaveis
+    $placeholdeKeys = array_map($createPlaceholder, $keys);
 
     $contentView = self::getContentView($view);
-    return $contentView;
+
+    $parseContentView = str_replace($placeholdeKeys, array_values($vars), $contentView);
+
+    return $parseContentView;
   }
 }
