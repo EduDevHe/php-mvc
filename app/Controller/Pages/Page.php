b7b66f99affa287ef3eaced172aeb078bbb531a5
Eduardo <edu.he.dev@gmail.com>
2023-07-21 16:39:36 -0300

    add templates

--- app/Controller/Pages/Page.php
+++ app/Controller/Pages/Page.php
@@ -8,6 +8,27 @@ class Page
 {
 
   /**
+   * Metodo responsavel por retornar uma View do Header 
+   *
+   *
+   * @return string
+   **/
+  private static function getHeader()
+  {
+    return View::render('pages/header');
+  }
+
+  /**
+   * Metodo responsavel por retornar uma View do Header 
+   *
+   *
+   * @return string
+   **/
+  private static function getFooter()
+  {
+    return View::render('pages/footer');
+  }
+  /**
    * 
    *
    * Método responsavel por retornar o conteúdo de uma pagina
@@ -18,6 +39,6 @@ class Page
    **/
   public static function getPage(string $title, string $content): string
   {
-    return View::render("pages/page", ['title' => $title, 'content' => $content]);
+    return View::render("pages/page", ['title' => $title, 'header' => self::getHeader(), 'content' => $content, 'footer' => self::getFooter()]);
   }
 }
